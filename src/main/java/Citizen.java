8c98d6d3ef9d1f75c52e1bb706f9008c947a7c97
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2018-06-27 20:28:28 -0300

    Creating thread simulator and citizen thread

---
+++ src/main/java/Citizen.java
@@ -0,0 +1,23 @@
+package main.java;
+
+public class Citizen extends Thread {
+
+    private String tid;
+
+    public Citizen(String tid) {
+        this.tid = tid;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public String getTid() {
+        return tid;
+    }
+
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+}
---
+++ src/main/java/Simulator.java
@@ -0,0 +1,45 @@
+package main.java;
+
+import java.util.*;
+
+public class Simulator {
+
+    private static int nThreads0P;
+    private static int nThreads1P;
+    private static int nThreads2P;
+    private Random rand;
+
+    public Simulator() {
+        nThreads0P = 0;
+        nThreads1P = 0;
+        nThreads2P = 0;
+        rand = new Random();
+    }
+
+    public void generateThreads(int threadNumber) {
+        Citizen[] citizens = new Citizen[threadNumber];
+        for (int i = 0; i < citizens.length; i++) {
+            citizens[i] = new Citizen(generateRandom());
+            citizens[i].start();
+        }
+    }
+
+    public String generateRandom() {
+        int id = rand.nextInt(3);
+        String tid = id + "P";
+        switch (id) {
+            case 0:
+                nThreads0P++;
+                tid += nThreads0P;
+                break;
+            case 1:
+                nThreads1P++;
+                tid += nThreads1P;
+            case 2:
+                nThreads2P++;
+                tid += nThreads2P;
+        }
+        return tid;
+    }
+
+}
